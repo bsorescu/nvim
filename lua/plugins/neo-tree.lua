416dd35df755c7ded83226ae7e992af96069d8a8
bsorescu <bsorescu@gmail.com>
2024-12-18 21:20:03 +0000

    Initial commit for Neovim configuration

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,25 @@
+return  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    },
+    
+    config = function()
+      vim.keymap.set('n', '<C-n>', ':Neotree filesystem toggle left<CR>', {})
+      vim.keymap.set('n', '<leader>th', function()
+        vim.cmd('Neotree filesystem toggle')
+      end, { noremap = true, silent = true, desc = "Toggle Neo-tree hidden files" })
+      filesystem = {
+        filtered_items = {
+            visible = true,      -- Show hidden files
+            hide_dotfiles = false, -- Do not hide dotfiles (files starting with .)
+            hide_gitignored = false, -- Show git-ignored files
+        },
+      }
+    end
+  }
+
+
